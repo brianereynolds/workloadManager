14e7c2b4fc455257f89e4d755e652ac021b9e624
Brian Reynolds <breyno11@its.jnj.com>
2025-02-14 22:21:21 +0000

    Improve logging Move functions to to scheduling package

--- internal/controller/workloadmanager_controller.go
+++ internal/controller/workloadmanager_controller.go
@@ -21,6 +21,7 @@ import (
 	"errors"
 	"github.com/brianereynolds/k8smanagers_utils"
 	k8smanagersv1 "greyridge.com/workloadManager/api/v1"
+	"greyridge.com/workloadManager/internal/controller/monitoring"
 	"greyridge.com/workloadManager/internal/controller/scheduling"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
@@ -257,12 +258,11 @@ func (r *WorkloadManagerReconciler) apply(ctx context.Context, wlManager *k8sman
 		}
 
 		if procedure.Type == k8smanagersv1.StatefulSet {
-			err = r.updateAffinity(ctx, clientset, procedure, k8smanagersv1.StatefulSet)
+			err = r.updateScheduling(ctx, clientset, procedure, k8smanagersv1.StatefulSet)
 		}
 
 		if procedure.Type == k8smanagersv1.Deployment {
-
-			err = r.updateAffinity(ctx, clientset, procedure, k8smanagersv1.Deployment)
+			err = r.updateScheduling(ctx, clientset, procedure, k8smanagersv1.Deployment)
 		}
 
 		if err != nil {
@@ -273,7 +273,7 @@ func (r *WorkloadManagerReconciler) apply(ctx context.Context, wlManager *k8sman
 	return nil
 }
 
-func (r *WorkloadManagerReconciler) updateAffinity(ctx context.Context, clientset *kubernetes.Clientset, procedure k8smanagersv1.Procedure, wlType string) error {
+func (r *WorkloadManagerReconciler) updateScheduling(ctx context.Context, clientset *kubernetes.Clientset, procedure k8smanagersv1.Procedure, wlType string) error {
 	l := log.Log
 
 	var deployment *appsv1.Deployment
@@ -294,9 +294,11 @@ func (r *WorkloadManagerReconciler) updateAffinity(ctx context.Context, clientse
 			}
 
 			if scheduling.HasAffinity(statefulset) {
+				l.V(1).Info("Statefulset has Affinity", "Key", procedure.Affinity.Key, "Target", procedure.Affinity.Target)
 				statefulset.Spec.Template.Spec.Affinity.NodeAffinity = scheduling.CreateNodeAffinity(procedure.Affinity.Key, procedure.Affinity.Target)
 			}
 			if scheduling.HasSelector(statefulset) {
+				l.V(1).Info("Statefulset has Selector", "Key", procedure.Selector.Key, "Target", procedure.Selector.Target)
 				statefulset.Spec.Template.Spec.NodeSelector = scheduling.CreateNodeSelector(procedure.Selector.Key, procedure.Selector.Target)
 			}
 
@@ -315,11 +317,11 @@ func (r *WorkloadManagerReconciler) updateAffinity(ctx context.Context, clientse
 				return err
 			}
 			if scheduling.HasAffinity(deployment) {
-				l.V(1).Info("Has Affinity", "Key", procedure.Affinity.Key, "Target", procedure.Affinity.Target)
+				l.V(1).Info("Deployment has Affinity", "Key", procedure.Affinity.Key, "Target", procedure.Affinity.Target)
 				deployment.Spec.Template.Spec.Affinity.NodeAffinity = scheduling.CreateNodeAffinity(procedure.Affinity.Key, procedure.Affinity.Target)
 			}
 			if scheduling.HasSelector(deployment) {
-				l.V(1).Info("Has Selector", "Key", procedure.Selector.Key, "Target", procedure.Selector.Target)
+				l.V(1).Info("Deployment has Selector", "Key", procedure.Selector.Key, "Target", procedure.Selector.Target)
 				deployment.Spec.Template.Spec.NodeSelector = scheduling.CreateNodeSelector(procedure.Selector.Key, procedure.Selector.Target)
 			}
 
@@ -346,104 +348,11 @@ func (r *WorkloadManagerReconciler) updateAffinity(ctx context.Context, clientse
 		timeout := time.Duration(procedure.Timeout) * time.Second
 		l.Info("Starting to wait", "name", workload, "timeout", timeout)
 		waitForConditionWithTimeout(func() bool {
-			return isResourceReady(ctx, wlType)
+			return monitoring.IsResourceReady(ctx, wlType)
 		}, interval, timeout)
 	}
 
 	return nil
-}
-
-func isResourceReady(ctx context.Context, wlType string) bool {
-	namespace := ctx.Value("namespace").(string)
-	clientset := ctx.Value("clientset").(*kubernetes.Clientset)
-
-	if wlType == k8smanagersv1.Deployment {
-		deployment := ctx.Value("resource").(*appsv1.Deployment)
-		return isDeploymentReady(clientset, namespace, deployment)
-	}
-	if wlType == k8smanagersv1.StatefulSet {
-		statefulset := ctx.Value("resource").(*appsv1.StatefulSet)
-		return isStatefulSetReady(clientset, namespace, statefulset)
-	}
-	return false
-}
-
-func isDeploymentReady(clientset *kubernetes.Clientset, namespace string, deployment *appsv1.Deployment) bool {
-	l := log.Log
-	l.Info("Waiting to start...", "name", deployment.Name)
-
-	mondeployment, err := clientset.AppsV1().Deployments(namespace).Get(context.Background(), deployment.Name, metav1.GetOptions{})
-	if err != nil {
-		l.Error(err, "Could not monitor")
-	}
-
-	labelSelector := metav1.FormatLabelSelector(mondeployment.Spec.Selector)
-	pods, err := getPodFromLabel(clientset, namespace, labelSelector)
-
-	var podname string = "UNKNOWN"
-	if len(pods.Items) > 0 {
-		podname = pods.Items[0].Name
-	}
-
-	// Get the pod
-	pod, err := clientset.CoreV1().Pods(namespace).Get(context.Background(), podname, metav1.GetOptions{})
-	if err != nil {
-		l.Error(err, "Could not find pod named", "podname", podname)
-	}
-
-	for _, condition := range pod.Status.Conditions {
-		if condition.Type == "Ready" && condition.Status == "True" {
-			creationTimestamp := pod.ObjectMeta.CreationTimestamp.Time
-			currentTime := time.Now()
-
-			l.Info("Pod started.", "name", deployment.Name, "start-up time", currentTime.Sub(creationTimestamp))
-			return true
-		}
-	}
-
-	return false
-}
-
-func isStatefulSetReady(clientset *kubernetes.Clientset, namespace string, statefulset *appsv1.StatefulSet) bool {
-	l := log.Log
-	l.Info("Waiting to start...", "name", statefulset.Name)
-
-	monstatefulset, err := clientset.AppsV1().StatefulSets(namespace).Get(context.Background(), statefulset.Name, metav1.GetOptions{})
-	if err != nil {
-		l.Error(err, "Could not monitor")
-	}
-
-	labelSelector := metav1.FormatLabelSelector(monstatefulset.Spec.Selector)
-	pods, err := getPodFromLabel(clientset, namespace, labelSelector)
-
-	// Print the status of each pod
-	for _, pod := range pods.Items {
-		if pod.DeletionTimestamp != nil {
-			// One of the pods in the stateful set is term
-			l.Info("Pod is terminating", "name", pod.Name)
-			return false
-		}
-	}
-
-	expectedReplicas := *monstatefulset.Spec.Replicas
-	readyReplicas := monstatefulset.Status.ReadyReplicas
-	l.Info("Monitoring replicas", "expected", expectedReplicas, "ready", readyReplicas)
-	if readyReplicas == expectedReplicas {
-		l.Info("Statefulset ready.", "name", statefulset.Name)
-		return true
-	}
-	return false
-}
-
-func getPodFromLabel(clientset *kubernetes.Clientset, namespace string, labelSelector string) (*v1.PodList, error) {
-	// List the pods matching the label selector
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
-		LabelSelector: labelSelector,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return pods, nil
 }
 
 func waitForConditionWithTimeout(condFunc func() bool, interval, timeout time.Duration) bool {
